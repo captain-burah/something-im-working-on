d702cce5c5623d6b5cf3bd0b21ed39ca0e4d17fe
Captain-Burah <cptburah@gmail.com>
2023-12-11 16:12:43 +0400

    about mobile - content writing

--- resources/views/about/mobile/body.blade.php
+++ resources/views/about/mobile/body.blade.php
@@ -42,12 +42,9 @@
                 />
                 </div>
                 <div
-                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000 leading-6 mt-4"
                 >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                    At ESNAAD, we are guided by our core values, which define who we are and what we do. With Integrity we act with honesty, transparency, and accountability in all our dealings with our customers, partners, employees, and society following and pursuing excellence in everything we do, from the quality of our developments and services, to the performance of our teams, to the satisfaction of our stakeholders. Embracing innovation as a way to create value, solve problems, and improve our processes and developments while caring about the environment and the future generations in sustainability that engages to improving community well-being through planned development and community-building programs and maintaining the highest standards of honesty and openness in all of our relationships.
                 </div>
             </div>
 
@@ -65,12 +62,9 @@
                 />
                 </div>
                 <div
-                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000 leading-6 mt-4"
                 >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                    Our mission is to create innovative and sustainable living spaces that enhance the quality of life of our stakeholders and investors. We aim to achieve this by empowering dreams, building legacies while redefining luxury living in the heart of Dubai. With a commitment to excellence, we envision and create dynamic communities that inspire a lifestyle of distinction working with talented and professional partners that share our vision and values, and that provide us with the best expertise and resources in the industry.
                 </div>
             </div>
 
@@ -88,12 +82,9 @@
                 />
                 </div>
                 <div
-                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+                class="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000 leading-6 mt-4"
                 >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                    Our vision is to be the region’s leading premium real estate development and investment company, with a Design, Quality and Excellence whilst, Developing our organization and culture, and fostering a work environment that attracts, motivates, and retains the best talent in the industry. We innovate and improve our processes and developments, to adopt to the latest trends in technologies and best practices as to the code of conduct within the real estate realm.
                 </div>
             </div>
         </div>

55f53f20e266418402acb84b5d0386bfd0aff239
Captain-Burah <cptburah@gmail.com>
2023-12-20 14:32:53 +0400

    contact form mobile

--- resources/views/contact/mobile/body.blade.php
+++ resources/views/contact/mobile/body.blade.php
@@ -1,11 +1,17 @@
-<div class="sm:container sm:mx-auto mx-0 px-0 sm:px-4 my-20 sm:my-4 mx-auto">
-    <div class="container mx-auto my-16">
-        <form class="flex flex-col items-start w-full"  method="post" action="{{ url('/communities/register-your-interest') }}" id="contact-desktop-form">
+<div class="sm:container sm:mx-auto mx-0 px-0 sm:px-4 my-8 sm:my-4 mx-auto">
+    <div class="container mx-auto my-8">
+        @if (\Session::has('success'))
+            <div class="w-full bg-green-100 rounded-lg p-4">
+                <ul>
+                    <li>{!! \Session::get('success') !!}</li>
+                </ul>
+            </div>
+        @endif
+        <form class="flex flex-col items-start w-full"  method="post" action="{{ url('contact-form-post') }}" id="contact-desktop-form">
             @csrf
-               
             <div class="icon bg-black text-white w-6 h-6 absolute flex items-center justify-center p-5" style="left:-40px"><i class="fal fa-phone-volume fa-fw text-2xl transform -rotate-45"></i></div>
-            <h3 class="text-2xl text-gray-900 font-semibold">Lorem ipsum dolor sit amet,</h3>
-            <p class="text-gray-600 mb-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus augue ut ligula</p>
+            <h3 class="text-2xl text-gray-900 font-semibold">Reach out to us</h3>
+            <p class="text-gray-600 mb-4">We're delighted to assist you! Please use the form below to get in touch with our team. Whether you have questions, feedback, or inquiries, we're here to help.</p>
             <input type="hidden" name="country_code" id="country_code">
             <input type="text" name="name" id="name" placeholder="Full Name" class="border p-2  w-full">
             <input type="text" name="name" id="phone" placeholder="Phone Number" class="border p-2 mt-3  w-full">
@@ -58,67 +64,4 @@
 
 
     </script>
-
-    {{-- FORM SUBMIT --}}
-    <script>
-        /**
-         * INITIATE HEADERS WITH CSRF TOKENIZATION
-         * FOR FORM SUBMISSION
-         */
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="XSRF-TOKEN"]').attr('content')
-            }
-        });
-        $('#contact-desktop-form').on('submit', function(e){
-
-            e.preventDefault();
-
-            var name = $("#name").val();
-
-            var email = $("#email").val();
-
-            var phone = $("#phone").val();
-
-            var country_code = $("#country_code").val();
-
-            var msg = $("#msg").val();
-
-
-
-            /**
-             * INITIATE AN AJAX SCRIPT FOR THE FORM SUBMISSION
-             * ALONG WITH POST ROUTE METHOD AND URL. IF RESPONSE
-             * IS A SUCCESS DISPLAY THE THANK YOU MODAL AND
-             * UPDATE THE FORM SESSION IN SESSION-STORAGE OF BROWSER
-             *
-            */
-            $.ajax({
-                type:'POST',
-                url:"{{ URL('api/v1/esnaad/contact-form') }}",
-                headers : {
-                    'Authorizatio' : 'Bearer ' + 'schDv?8qat`6zLVZ;8Lwoy(g`/Asm%D88$a>7Wl,20amt[=uN1',
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                contentType : 'application/x-www-form-urlencoded',
-                data:{
-                    name:name,
-                    email:email,
-                    phone:phone,
-                    country_code:country_code,
-                    msg:msg,
-                    
-                },
-                error : function(xhr, status, error) {
-                    var err = eval("(" + xhr.responseText + ")");
-                }
-            }).done(function(data){  
-                alert('Successfully');  
-             }).fail(function(jqXHR, ajaxOptions, thrownError){  
-                alert(jqXHR.responseText);  
-            });  
-
-        });
-    </script>
-
 @endsection
